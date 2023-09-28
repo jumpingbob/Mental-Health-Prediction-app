88c4d29d688db54f7aec82b3d184dbf512a9882b
jumpingbob <145727481+jumpingbob@users.noreply.github.com>
2023-09-28 16:40:55 +0900

    最適化

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,22 @@ import pandas as pd
 import plotly.express as px
 
 # Excelファイルを読み込む
-#@st.cache  # データのキャッシュを有効にして高速化
+@st.cache
 def load_data():
-    df = pd.read_excel("questionnaire (1).xlsx")  # Excelファイルのパスを指定
+    df = pd.read_excel("questionnaire (1).xlsx")
     return df
+
+def calculate_avg_score(factor_data):
+    total_score = 0
+    for idx, row in factor_data.iterrows():
+        st.markdown(f"**{row['設問名']}**")
+        score = st.radio("回答", options1, key=row["設問名"])
+        if not pd.isna(row["反転"]):
+            score = 5 - int(score[0])
+        else:
+            score = int(score[0])
+        total_score += score
+    return total_score / len(factor_data)
 
 df = load_data()
 
@@ -16,46 +28,11 @@ st.title("ベイマックス")
 # ラジオボタンのデフォルト選択肢
 options1 = ["1 全くあてはまらない", "2 あまりあてはまらない", "3 少しあてはまる", "4 とてもあてはまる"]
 
-factors = df["因子名"]
-
-ningen_exsists = False
-sinri_exsists = False
-syokuzi_exsists = False
-
 # ラジオボタンで回答を収集し、因子ごとの平均点を計算
 factor_scores = {}
-for factor in factors:
-
-    if(factor == "人間関係"):
-        if( ningen_exsists ):
-            continue
-    if(factor == "心理的余裕"):
-        if( sinri_exsists ):
-            continue
-    if(factor == "食事・睡眠"):
-        if( syokuzi_exsists ):
-            continue
+for factor, factor_data in df.groupby("因子名"):
     st.subheader(factor)
-
-    if(factor == "人間関係"):
-        ningen_exsists = True
-    if(factor == "心理的余裕"):
-        sinri_exsists = True
-    if(factor == "食事・睡眠"):
-        syokuzi_exsists = True
-
-    factor_data = df[df["因子名"] == factor]
-    total_score = 0
-    for idx, row in factor_data.iterrows():
-        st.markdown("**" + row["設問名"] + "**")
-        score = st.radio("回答", options1, key=row["設問名"])
-        # 反転項目の場合、数値を反転
-        if str(row["反転"]) != "nan":
-            score = 5 - int(score[0])
-        else:
-            score = int(score[0])
-        total_score += score
-    avg_score = total_score / len(factor_data)
+    avg_score = calculate_avg_score(factor_data)
     factor_scores[factor] = avg_score
     st.write(f"{factor}の平均点: {avg_score:.2f}")
 
