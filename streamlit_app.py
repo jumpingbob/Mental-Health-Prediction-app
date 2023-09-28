2f3845debe6e91c40f88edc0af398c111f90e1bc
jumpingbob <145727481+jumpingbob@users.noreply.github.com>
2023-09-28 04:51:47 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,32 @@ df
 factors = df["因子名"]
 
 factors
+
+# ラジオボタンで回答を収集し、因子ごとの平均点を計算
+factor_scores = {}
+for factor in factors:
+    st.subheader(factor)
+    factor_data = df[df["因子名"] == factor]
+    total_score = 0
+    for idx, row in factor_data.iterrows():
+        st.write(row["設問名"])
+        score = st.radio("回答", options)
+        # 反転項目の場合、数値を反転
+        if row["反転"]:
+            score = 5 - int(score[0])
+        else:
+            score = int(score[0])
+        total_score += score
+    avg_score = total_score / len(factor_data)
+    factor_scores[factor] = avg_score
+    st.write(f"{factor}の平均点: {avg_score:.2f}")
+
+# レーダーチャートを描画
+if factor_scores:
+    st.subheader("因子ごとの評価")
+    fig = px.line_polar(
+        r=list(factor_scores.values()),
+        theta=list(factor_scores.keys()),
+        line_close=True
+    )
+    st.plotly_chart(fig)
