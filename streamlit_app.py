2f1bcff2463ab8ff1d27af91f5fbe12847278040
stream.T <145727481+jumpingbob@users.noreply.github.com>
2024-05-16 05:42:27 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,17 +13,20 @@ def get_user_input(features):
 def min_max_scaling(user_input):
     min_val = np.min(user_input)
     max_val = np.max(user_input)
+    st.write(f"min_val: {min_val}, max_val: {max_val}")
     if min_val == max_val:
-        # すべての値が同じ場合、すべてのスケーリング値を0.5に設定
         scaled_values = [0.5 for _ in user_input]
     else:
         scaled_values = [(x - min_val) / (max_val - min_val) for x in user_input]
+    st.write(f"scaled_values: {scaled_values}")
     return scaled_values
 
 # ストレスレベルを計算
 def calculate_stress_level(scaled_values, feature_importances):
     stress_level = np.dot(scaled_values, feature_importances)
+    st.write(f"stress_level: {stress_level}")
     return stress_level
+
 
 # Streamlitアプリの実行
 def main():
